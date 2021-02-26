e616ef2f6cada574a8181a2a5a28d494495e30d2
Sebastian Schmidt <c-raute@outlook.com>
2021-02-26 15:50:10 +0100

    Factorial

--- MathLib/MathLib.cs
+++ MathLib/MathLib.cs
@@ -1,8 +1,10 @@
-﻿using System;
-
-namespace MathLib
+﻿namespace MathLib
 {
     public class MathLib
     {
+        public ulong Factorial(uint n)
+        {
+            return n == 0 ? 1 : n * Factorial(n - 1);
+        }
     }
 }
--- MathLibTests/UnitTest1.cs
+++ MathLibTests/UnitTest1.cs
@@ -2,12 +2,45 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MathLibTests
 {
+    /// <summary>
+    /// https://docs.microsoft.com/de-de/visualstudio/test/quick-start-test-driven-development-with-test-explorer?view=vs-2019
+    /// </summary>
     [TestClass]
     public class UnitTest1
     {
+        private uint[] _expectedResults = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800 };
+
         [TestMethod]
-        public void TestMethod1()
+        public void BasicFactorialTest()
         {
+            // Create an instance to test:
+            var mathLib = new MathLib.MathLib();
+
+            // Define a test input and output value:
+            var expectedResult = 1;
+            var input = 0u;
+
+            // Run the method under test:
+            var actualResult = mathLib.Factorial(input);
+
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, expectedResult / 1000);
+        }
+
+        [TestMethod]
+        public void FactorialValueRangeTest()
+        {
+            // Create an instance to test.
+            var mathLib = new MathLib.MathLib();
+
+            // Try a range of values.
+            for (var i = 0u; i < _expectedResults.Length; i++)
+            {
+                // Run the method under test:
+                var actualResult = mathLib.Factorial(i);
+                // Verify the result:
+                Assert.AreEqual(_expectedResults[i], actualResult, delta: _expectedResults[i] / 1000);
+            }
         }
     }
 }
