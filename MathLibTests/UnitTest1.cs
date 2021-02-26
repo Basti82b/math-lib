5523825ac1cbe6620b44465c440fdea16cf3fd6b
Sebastian Schmidt <c-raute@outlook.com>
2021-02-26 14:18:36 +0100

    Initial commit

---
+++ MathLibTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathLibTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
