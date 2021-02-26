5348d7e51fa003c0cb99e84936e4a75709358d8d
Sebastian Schmidt <c-raute@outlook.com>
2021-02-26 15:50:10 +0100

    Steer MathLib via constructor

--- MathLib/MathLib.cs
+++ MathLib/MathLib.cs
@@ -4,7 +4,24 @@ namespace MathLib
 {
     public class MathLib
     {
+        private bool _recursive;
+
+        public MathLib(bool recursive = false)
+        {
+            _recursive = recursive;
+        }
+
         public ulong Factorial(uint n)
+        {
+            return _recursive ? RecursiveFactorial(n) : LINQFactorial(n);
+        }
+
+        private ulong RecursiveFactorial(uint n)
+        {
+            return n == 0 ? 1 : n * RecursiveFactorial(n - 1);
+        }
+
+        private ulong LINQFactorial(uint n)
         {
             return n == 0 ? 1 : (ulong)Enumerable.Range(1, (int)n).Aggregate((i, j) => i * j);
         }
--- MathLibTests/UnitTest1.cs
+++ MathLibTests/UnitTest1.cs
@@ -9,12 +9,13 @@ namespace MathLibTests
     public class UnitTest1
     {
         private uint[] _expectedResults = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800 };
+        private bool _recursive;
 
         [TestMethod]
         public void BasicFactorialTest()
         {
             // Create an instance to test:
-            var mathLib = new MathLib.MathLib();
+            var mathLib = new MathLib.MathLib(_recursive);
 
             // Define a test input and output value:
             var expectedResult = 1;
@@ -31,7 +32,7 @@ namespace MathLibTests
         public void FactorialValueRangeTest()
         {
             // Create an instance to test.
-            var mathLib = new MathLib.MathLib();
+            var mathLib = new MathLib.MathLib(_recursive);
 
             // Try a range of values.
             for (var i = 0u; i < _expectedResults.Length; i++)
@@ -41,6 +42,20 @@ namespace MathLibTests
                 // Verify the result:
                 Assert.AreEqual(_expectedResults[i], actualResult, delta: _expectedResults[i] / 1000);
             }
+        }
+
+        [TestMethod]
+        public void RecursiveFactorialTest()
+        {
+            _recursive = true;
+            FactorialValueRangeTest();
+        }
+
+        [TestMethod]
+        public void LINQFactorialTest()
+        {
+            _recursive = false;
+            FactorialValueRangeTest();
         }
     }
 }
